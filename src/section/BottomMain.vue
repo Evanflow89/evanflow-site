db4859e86bf0a88df47dc02cfd209837c514cec2
Evanflow89 <ivanbaragone@gmail.com>
2022-11-10 11:31:21 +0100

    Added BottomMain

---
+++ src/section/BottomMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="BottomMain"></section>
+</template>
+
+<script>
+export default {
+  name: "BottomMain",
+};
+</script>
+
+<style lang="scss" scoped>
+.BottomMain {
+  height: 100vh;
+  background-color: black;
+}
+</style>
