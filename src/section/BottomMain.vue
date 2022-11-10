7d6b4d2e180fcea664c970a623fbc93653240b02
Evanflow89 <ivanbaragone@gmail.com>
2022-11-10 11:49:12 +0100

    Add Gsap bottomMain

--- src/section/BottomMain.vue
+++ src/section/BottomMain.vue
@@ -1,10 +1,36 @@
 <template>
-  <section class="BottomMain"></section>
+  <section class="BottomMain">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h2 class="why">Testo</h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export default {
   name: "BottomMain",
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".why", {
+      scrollTrigger: {
+        trigger: ".why",
+        start: "top center",
+        end: "top 200px",
+        scrub: 1,
+      },
+      y: 400,
+      ease: "none",
+      duration: 2,
+    });
+  },
 };
 </script>
 
@@ -12,5 +38,6 @@ export default {
 .BottomMain {
   height: 100vh;
   background-color: black;
+  color: white;
 }
 </style>
