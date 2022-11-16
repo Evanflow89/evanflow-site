dc221e51434bc5a7b58d732c5662d48b07f2ee43
Evanflow89 <ivanbaragone@gmail.com>
2022-11-16 13:24:22 +0100

    remove gsap under 1000

--- src/section/TopMain.vue
+++ src/section/TopMain.vue
@@ -65,34 +65,38 @@ export default {
   mounted() {
     gsap.registerPlugin(ScrollTrigger);
 
-    let getRatio = (el) =>
-      window.innerHeight / (window.innerHeight + el.offsetHeight);
+    if (window.innerWidth > 1000) {
+      let getRatio = (el) =>
+        window.innerHeight / (window.innerHeight + el.offsetHeight);
 
-    gsap.utils.toArray("section").forEach((section, i) => {
-      section.bg = section.querySelector(".bg");
+      gsap.utils.toArray("section").forEach((section, i) => {
+        section.bg = section.querySelector(".bg");
 
-      // the first image (i === 0) should be handled differently because it should start at the very top.
-      // use function-based values in order to keep things responsive
-      gsap.fromTo(
-        section.bg,
-        {
-          backgroundPosition: () =>
-            i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px",
-        },
-        {
-          backgroundPosition: () =>
-            `50% ${window.innerHeight * (1 - getRatio(section))}px`,
-          ease: "none",
-          scrollTrigger: {
-            trigger: section,
-            start: () => (i ? "top bottom" : "top top"),
-            end: "bottom top",
-            scrub: true,
-            invalidateOnRefresh: true, // to make it responsive
+        // the first image (i === 0) should be handled differently because it should start at the very top.
+        // use function-based values in order to keep things responsive
+        gsap.fromTo(
+          section.bg,
+          {
+            backgroundPosition: () =>
+              i
+                ? `50% ${-window.innerHeight * getRatio(section)}px`
+                : "50% 0px",
           },
-        }
-      );
-    });
+          {
+            backgroundPosition: () =>
+              `50% ${window.innerHeight * (1 - getRatio(section))}px`,
+            ease: "none",
+            scrollTrigger: {
+              trigger: section,
+              start: () => (i ? "top bottom" : "top top"),
+              end: "bottom top",
+              scrub: true,
+              invalidateOnRefresh: true, // to make it responsive
+            },
+          }
+        );
+      });
+    }
   },
 };
 </script>
