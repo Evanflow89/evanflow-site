0c66c867106ea0c6db044a8384ecd10598de5045
Evanflow89 <ivanbaragone@gmail.com>
2022-11-16 16:49:41 +0100

    1000px and <576 ok

--- src/section/BottomMain.vue
+++ src/section/BottomMain.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <div class="row d-flex justify-content-center">
           <div class="col-8 text-center why">
-            <h2>Ottieni Visibilità</h2>
+            <h2 class="title text-start">Ottieni Visibilità</h2>
             <p class="pt-4">
               Qualsiasi sia la tua attività, che si tratti di un bar, un
               ristorante o un'azienda, è importante avere una presenza ben
@@ -23,11 +23,45 @@
 import BottomMain2 from "../common/BottomMain2.vue";
 import ContactUs from "../common/ContactUs.vue";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export default {
   name: "BottomMain",
   components: {
     BottomMain2,
     ContactUs,
+  },
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    if (window.innerWidth > 1000) {
+      gsap.to(".title", {
+        scrollTrigger: {
+          trigger: ".title",
+          start: "top center",
+          end: "top center",
+          once: true,
+          scrub: 1,
+        },
+        x: 280,
+        ease: "none",
+        duration: 1,
+      });
+    } else {
+      gsap.to(".title", {
+        scrollTrigger: {
+          trigger: ".title",
+          start: "top center",
+          end: "top center",
+          once: true,
+          scrub: 1,
+        },
+        x: 40,
+        ease: "none",
+        duration: 1,
+      });
+    }
   },
 };
 </script>
